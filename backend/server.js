bd80dd35cf3e2e0c37314fedf8475c482fc1a0c0
666lexa666 <konoplevlesa5@gmail.com>
2025-09-17 03:29:19 +0300

    Update 1.6

--- backend/server.js
+++ backend/server.js
@@ -10,7 +10,9 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 // Middleware
-app.use(cors());
+app.use(cors({
+  origin: '*' // или укажи домен фронта: 'https://myfrontend.vercel.app'
+}));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
